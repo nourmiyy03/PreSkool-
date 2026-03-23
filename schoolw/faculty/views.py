bb2c9da6edff2c8dfbc10f91ae6800c6c77499fb
Wafae Achahbar <wafaeachahbar2@gmail.com>
2026-03-23 23:35:13 +0100

    Ajout du suit du projet dans une nouvelle branche

---
+++ schoolw/faculty/views.py
@@ -0,0 +1,7 @@
+
+from django.shortcuts import render, redirect
+
+def index(request):
+    return render(request, 'authentication/login.html')
+def dashboard(request):
+    return render(request, 'students/student-dashboard.html')
---
+++ schoolw/home_auth/views.py
@@ -0,0 +1,78 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from .models import CustomUser
+
+# Create your views here.
+
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from .models import CustomUser
+
+# Create your views here.
+
+def signup_view(request):
+    if request.method == 'POST':
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        email = request.POST['email']
+        password = request.POST['password']
+        role = request.POST.get('role')  # student, teacher ou admin
+
+        # Créer l'utilisateur
+        user = CustomUser.objects.create_user(
+            username=email,
+            email=email,
+            first_name=first_name,
+            last_name=last_name,
+            password=password,
+        )
+
+        # Assigner le rôle
+        if role == 'student':
+            user.is_student = True
+        elif role == 'teacher':
+            user.is_teacher = True
+        elif role == 'admin':
+            user.is_admin = True
+
+        user.save()
+        login(request, user)
+        messages.success(request, 'Signup successful!')
+        return redirect('index')
+
+    return render(request, 'authentication/register.html')
+    
+
+def login_view(request):
+    if request.method == 'POST':
+        email = request.POST['email']
+        password = request.POST['password']
+        user = authenticate(request, username=email, password=password)
+
+        if user is not None:
+            login(request, user)
+            messages.success(request, 'Login successful!')
+
+            # Redirection selon le rôle
+            if user.is_admin:
+                return redirect('admin_dashboard')
+            elif user.is_teacher:
+                return redirect('teacher_dashboard')
+            elif user.is_student:
+                return redirect('dashboard')
+            else:
+                messages.error(request, 'Invalid user role')
+                return redirect('index')
+
+        else:
+            messages.error(request, 'Invalid credentials')
+            return redirect('index')
+
+    return render(request, 'authentication/login.html')
+
+def logout_view(request):
+    logout(request)
+    messages.success(request, 'You have been logged out.')
+    return redirect('index')
